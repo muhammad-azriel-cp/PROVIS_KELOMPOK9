c9cb29258ae0704dbf01bb56b734cb38dba22dac
RizalAmriH <99193504+RizalAmriH@users.noreply.github.com>
2022-04-09 22:27:55 +0700

    Add files via upload

---
+++ lib/home.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
+
+  List<String> datastatistik = [
+    "Total Mahasiswa",
+    "Total Dosen",
+    "Total Fakultas",
+    "Total Prodi"
+  ];
+  List<int> angka = [40000, 300, 12, 120];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Stack(children: <Widget>[
+              //BG(Layer 1)
+              Container(
+                margin: const EdgeInsets.only(top: 0),
+                height: 300,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/bg.png"),
+                      fit: BoxFit.cover),
+                ),
+              ),
+              Column(children: <Widget>[
+                //Yang diatas backgroud gambar
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(
+                      left: 20, top: 50, right: 20, bottom: 0),
+                  child: const Text("Jum'at , 01 April 2022",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Welcome Back!",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 150, left: 30, right: 30),
+                  width: double.infinity,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Center(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.clear),
+                            onPressed: () {
+                              /* Clear the search field */
+                            },
+                          ),
+                          hintText: 'Search...',
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ])
+            ]),
+            Container(
+              margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: (1 / .4),
+                shrinkWrap: true,
+                children: List.generate(angka.length, (index) {
+                  return Card(
+                    color: Colors.blueAccent,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          //leading: Icon(Icons.album, size: 45),
+                          title: Text(
+                            datastatistik[index],
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.white70),
+                          ),
+                          subtitle: Text(angka[index].toString(),
+                              style:
+                                  TextStyle(fontSize: 24, color: Colors.white)),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 30, top: 20),
+              alignment: Alignment.centerLeft,
+              child: const Text("News Update",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black)),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 30, right: 30),
+              child: GridView.count(
+                crossAxisCount: 1,
+                childAspectRatio: (1 / .4),
+                shrinkWrap: true,
+                children: List.generate(angka.length, (index) {
+                  return Card(
+                    color: Colors.blueAccent,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          //leading: Icon(Icons.album, size: 45),
+                          title: Text(
+                            "Ini Judul Berita",
+                            style: TextStyle(fontSize: 24, color: Colors.white70),
+                          ),
+                          subtitle: Text("Ini deskripsinya",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white)),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+                
+              ),
+            ),
+          ]),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school_outlined),
+              label: 'Dosen',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business_rounded),
+              label: 'Fakultas',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_outlined),
+              label: 'Mahasiswa',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_sharp),
+              label: 'Universitas',
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Colors.redAccent,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+        ),
+      ),
+    );
+  }
+}
