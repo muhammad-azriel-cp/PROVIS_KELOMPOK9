a1b4157483a4b99f73d2c026d38e0abddbd7f8d1
RizalAmriH <99193504+RizalAmriH@users.noreply.github.com>
2022-04-22 16:26:25 +0700

    Add files via upload

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
-class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
+
+class Home extends StatefulWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
 
   List<String> datastatistik = [
     "Total Mahasiswa",
@@ -12,6 +19,7 @@ class Home extends StatelessWidget {
   ];
   List<int> angka = [40000, 300, 12, 120];
 
+  final int _currentindex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,15 +40,22 @@ class Home extends StatelessWidget {
               ),
               Column(children: <Widget>[
                 //Yang diatas backgroud gambar
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(
-                      left: 20, top: 50, right: 20, bottom: 0),
-                  child: const Text("Jum'at , 01 April 2022",
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
+                Row(
+
+                  children: <Widget>[
+                    Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(
+                        left: 20, top: 50, right: 20, bottom: 0),
+                    child: const Text("Jum'at , 01 April 2022",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                  ),
+                 
+                  ]
+                   
                 ),
                 Container(
                   margin: const EdgeInsets.only(left: 20),
@@ -57,7 +72,7 @@ class Home extends StatelessWidget {
                   height: 40,
                   decoration: BoxDecoration(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(5)),
+                      borderRadius: BorderRadius.circular(8)),
                   child: Center(
                     child: TextField(
                       decoration: InputDecoration(
@@ -75,8 +90,17 @@ class Home extends StatelessWidget {
                 ),
               ])
             ]),
+              Container(
+              margin: const EdgeInsets.only(left: 30, top: 20),
+              alignment: Alignment.centerLeft,
+              child: const Text("Statistik",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black)),
+            ),
             Container(
-              margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+              margin: const EdgeInsets.only(left: 30, right: 30),
               child: GridView.count(
                 crossAxisCount: 2,
                 childAspectRatio: (1 / .4),
@@ -84,6 +108,7 @@ class Home extends StatelessWidget {
                 children: List.generate(angka.length, (index) {
                   return Card(
                     color: Colors.blueAccent,
+                    
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
@@ -114,7 +139,7 @@ class Home extends StatelessWidget {
                       color: Colors.black)),
             ),
             Container(
-              margin: const EdgeInsets.only(top: 20,left: 30, right: 30),
+              margin: const EdgeInsets.only( left: 30, right: 30),
               child: GridView.count(
                 crossAxisCount: 1,
                 childAspectRatio: (1 / .4),
@@ -124,12 +149,14 @@ class Home extends StatelessWidget {
                     color: Colors.blueAccent,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
+                      // ignore: prefer_const_literals_to_create_immutables
                       children: <Widget>[
                         ListTile(
                           //leading: Icon(Icons.album, size: 45),
                           title: Text(
                             "Ini Judul Berita",
-                            style: TextStyle(fontSize: 24, color: Colors.white70),
+                            style:
+                                const TextStyle(fontSize: 24, color: Colors.white70),
                           ),
                           subtitle: Text("Ini deskripsinya",
                               style:
@@ -139,7 +166,6 @@ class Home extends StatelessWidget {
                     ),
                   );
                 }),
-                
               ),
             ),
           ]),
@@ -147,32 +173,43 @@ class Home extends StatelessWidget {
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.home_rounded),
+              icon: Icon(Icons.home_rounded, size: 30),
               label: 'Home',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.school_outlined),
+              icon: Icon(Icons.school_outlined, size: 32),
               label: 'Dosen',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.business_rounded),
+              icon: Icon(Icons.business_rounded, size: 32),
               label: 'Fakultas',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.people_outlined),
+              icon: Icon(Icons.people_outlined, size: 32),
               label: 'Mahasiswa',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance_sharp),
+              icon: Icon(Icons.account_balance_sharp, size: 32),
               label: 'Universitas',
             ),
           ],
-          currentIndex: 0,
+          onTap: (index) {
+            setState(() {
+              index = _currentindex;
+            });
+          },
+          currentIndex: _currentindex,
           selectedItemColor: Colors.redAccent,
           unselectedItemColor: Colors.grey,
           showUnselectedLabels: true,
+          //onTap: _changeSelectedNavBar,
         ),
       ),
+      
     );
   }
 }
+
+
+
+  
