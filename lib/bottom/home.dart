87eaa3e67ce656910b59e049e80d75a7313ee5db
RizalAmriH <99193504+RizalAmriH@users.noreply.github.com>
2022-05-08 20:58:40 +0700

    Add files via upload

--- lib/bottom/home.dart
+++ lib/bottom/home.dart
@@ -1,66 +1,45 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  _HomeState createState() => _HomeState();
+  State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   List<String> datastatistik = [
     "Total Mahasiswa",
     "Total Dosen",
     "Total Fakultas",
     "Total Prodi"
   ];
+
   List<int> angka = [40000, 300, 12, 120];
-  // This widget is the root of your application.
+
+  final int _currentindex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        
         body: SingleChildScrollView(
-          child: Column(
-          children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: new AssetImage("assets/images/img-dashboard.png"),
-                fit: BoxFit.fitWidth,
-              )
-            ),
-            child: Column(children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 50),
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, 
-              children: [
+          child: Column(children: <Widget>[
+            Stack(children: <Widget>[
+              //BG(Layer 1)
               Container(
-                    child: Text(
-                      "Welcome Back !",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        ),
-                      ),
+                margin: const EdgeInsets.only(top: 0),
+                height: 300,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/bg.png"),
+                      fit: BoxFit.cover),
+                ),
               ),
-              Container(
-                  child: Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-              ),
-              ]),
-              
-              // 
-            ),
-            Container(
-
-              child:Container(
+              Column(children: <Widget>[
+                //Yang diatas backgroud gambar
+                Row(children: <Widget>[
+                  Container(
                     alignment: Alignment.centerLeft,
                     margin: const EdgeInsets.only(
                         left: 20, top: 50, right: 20, bottom: 0),
@@ -70,9 +49,25 @@ class _HomeState extends State<Home> {
                             fontWeight: FontWeight.bold,
                             color: Colors.white)),
                   ),
-                   
-            ),
-            Container(
+                  Container(
+                    margin: const EdgeInsets.only(top:50, right:50),
+                    child: Icon(
+                      Icons.notifications,
+                      color: Colors.white,
+                      size: 20.0,
+                    ),
+                  ),
+                ]),
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Welcome Back!",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Container(
                   margin: const EdgeInsets.only(top: 150, left: 30, right: 30),
                   width: double.infinity,
                   height: 40,
@@ -94,15 +89,8 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ),
-            Container(
-              margin: const EdgeInsets.only(left: 30, top: 20),
-              alignment: Alignment.centerLeft,
-              child: const Text("Statistik",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)),
-            ),
+              ])
+            ]),
             Container(
               margin: const EdgeInsets.only(left: 30, right: 30),
               child: GridView.count(
@@ -112,7 +100,6 @@ class _HomeState extends State<Home> {
                 children: List.generate(angka.length, (index) {
                   return Card(
                     color: Colors.blueAccent,
-                    
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
@@ -143,28 +130,30 @@ class _HomeState extends State<Home> {
                       color: Colors.black)),
             ),
             Container(
-              margin: const EdgeInsets.only( left: 30, right: 30),
+              margin: const EdgeInsets.only(left: 30, right: 30),
               child: GridView.count(
                 crossAxisCount: 1,
                 childAspectRatio: (1 / .4),
                 shrinkWrap: true,
                 children: List.generate(angka.length, (index) {
                   return Card(
-                    color: Colors.blueAccent,
+                    margin: const EdgeInsets.only(top: 16),
+                    color: Colors.white,
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
-                      // ignore: prefer_const_literals_to_create_immutables
                       children: <Widget>[
                         ListTile(
-                          //leading: Icon(Icons.album, size: 45),
                           title: Text(
-                            "Ini Judul Berita",
+                            " Judul Berita",
                             style:
-                                const TextStyle(fontSize: 24, color: Colors.white70),
+                                TextStyle(fontSize: 24, color: Colors.black87),
                           ),
-                          subtitle: Text("Ini deskripsinya",
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.white)),
+                          subtitle: Text("Deskripsi",
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.black26)),
+                          leading: CircleAvatar(
+                            backgroundImage: AssetImage("assets/images/bg.png"),
+                          ),
                         ),
                       ],
                     ),
@@ -173,11 +162,9 @@ class _HomeState extends State<Home> {
               ),
             ),
           ]),
-          ),
-          ]),
         ),
+        
       ),
     );
   }
 }
-
