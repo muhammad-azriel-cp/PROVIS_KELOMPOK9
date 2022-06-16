62ae747812f1b0d9099425af548a4a20ebd7ac06
Annisa Muja Ahidah <80079890+annisamujaa@users.noreply.github.com>
2022-06-16 21:24:56 +0700

    Update dosen.dart

--- lib/bottom/dosen.dart
+++ lib/bottom/dosen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart' ;
 
 class Dosen extends StatefulWidget {
   const Dosen({Key? key}) : super(key: key);
@@ -15,8 +14,45 @@ class _DosenState extends State<Dosen> {
     "Total Dosen S2",
     "Total Dosen S3"
   ];
-
   List<int> angka = [500, 2310, 300, 200];
+
+// method untuk menampilkan data dosen terbaik
+  Card componentCard(String namaDosen, String jumlahPenelitian,
+      String programStudi, bool booleanWarna) {
+    return Card(
+      color: (booleanWarna) ? Colors.blueAccent : Colors.teal[50],
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[
+          Container(
+            child: ListTile(
+              leading: Icon(Icons.school_outlined, size: 40),
+              trailing: Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: Text(jumlahPenelitian + " Penelitian",
+                    style: TextStyle(
+                        fontSize: 14,
+                        color: (booleanWarna) ? Colors.white : Colors.black)),
+              ),
+              title: Text(
+                namaDosen,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: (booleanWarna) ? Colors.white : Colors.black),
+              ),
+              subtitle: Text(
+                programStudi,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: (booleanWarna) ? Colors.white : Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   final int _currentindex = 0;
   @override
@@ -26,7 +62,7 @@ class _DosenState extends State<Dosen> {
         body: SingleChildScrollView(
           child: Column(children: <Widget>[
             Stack(children: <Widget>[
-              //BG(Layer 1)
+              //poster
               Container(
                 margin: const EdgeInsets.only(top: 0),
                 height: 300,
@@ -38,32 +74,37 @@ class _DosenState extends State<Dosen> {
                 ),
               ),
               Column(children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.only(top: 50),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Container(
-                                child: Text(
-                                  "INFORMASI DOSEN",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 30,
-                                  ),
-                                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Text(
+                              "INFORMASI DOSEN",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
                               ),
-                            ]))
-                  ])
+                            ),
+                          ),
+                        ]))
+              ])
             ]),
+            // container yang berisi data jumlah dosen dan penelitiannya
             Container(
-              margin: const EdgeInsets.only(left: 30, top: 20),
-              alignment: Alignment.centerLeft,
-              child: const Text("Statistik",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black)),
+              margin: const EdgeInsets.only(left: 30, top: 20, bottom: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text("Data Dosen",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black)),
+                ],
+              ),
             ),
             Container(
               margin: const EdgeInsets.only(left: 30, right: 30),
@@ -74,21 +115,21 @@ class _DosenState extends State<Dosen> {
                 children: List.generate(angka.length, (index) {
                   return Card(
                     color: Colors.blueAccent,
-                    
                     child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ListTile(
-                          //leading: Icon(Icons.album, size: 45),
-                          title: Text(
-                            datastatistik[index],
-                            style:
-                                TextStyle(fontSize: 16, color: Colors.white70),
-                          ),
-                          subtitle: Text(angka[index].toString(),
-                              style:
-                                  TextStyle(fontSize: 24, color: Colors.white)),
-                        ),
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                datastatistik[index],
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              ),
+                              Text(angka[index].toString(),
+                                  style: TextStyle(
+                                      fontSize: 24, color: Colors.white)),
+                            ])
                       ],
                     ),
                   );
@@ -96,119 +137,39 @@ class _DosenState extends State<Dosen> {
               ),
             ),
             Container(
-              margin: const EdgeInsets.only(left: 30, top: 20),
+              margin: const EdgeInsets.only(left: 35, top: 20, bottom: 20),
               alignment: Alignment.centerLeft,
               child: const Text("Dosen Terbaik",
                   style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: Colors.black)),
             ),
+            // data dosen terbaik
             Container(
-              margin: const EdgeInsets.only( left: 30, right: 30),
+              margin: const EdgeInsets.only(left: 30, right: 30),
               child: GridView.count(
                 crossAxisCount: 1,
                 childAspectRatio: (1 / .2),
                 shrinkWrap: true,
                 children: [
-                  Card(
-                    color: Colors.blueAccent,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: <Widget>[
-                        Container(
-                          child: ListTile(
-                            leading: Icon(Icons.school_outlined, size: 40),
-                            trailing: new Text("30 Penelitian", style: TextStyle(color: Colors.white),),
-                            title: Text(
-                              "Prof. Dr. Munir, M. IT.",
-                              style:
-                                  const TextStyle( fontSize: 16, color: Colors.white70),
-                            ),
-                            subtitle: Text("Ilmu Komputer",
-                              style:
-                                  TextStyle(fontSize: 14, color: Colors.white)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Card(
-                    color: Colors.teal[50],
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: <Widget>[
-                        Container(
-                          child: ListTile(
-                            leading: Icon(Icons.school_outlined, size: 40),
-                            trailing: new Text("27 Penelitian", style: TextStyle(color: Colors.black),),
-                            title: Text(
-                              "Dr. Yudi Wibisono, M.T.",
-                              style:
-                                  const TextStyle( fontSize: 16, color: Colors.black54),
-                            ),
-                            subtitle: Text("Ilmu Komputer",
-                              style:
-                                  TextStyle(fontSize: 14, color: Colors.black)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Card(
-                    color: Colors.blueAccent,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: <Widget>[
-                        Container(
-                          child: ListTile(
-                            leading: Icon(Icons.school_outlined, size: 40),
-                            trailing: new Text("26 Penelitian", style: TextStyle(color: Colors.white),),
-                            title: Text(
-                              "Dr. Muhammad Nursalman, M.T.",
-                              style:
-                                  const TextStyle( fontSize: 16, color: Colors.white70),
-                            ),
-                            subtitle: Text("Ilmu Komputer",
-                              style:
-                                  TextStyle(fontSize: 14, color: Colors.white)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                   Card(
-                    color: Colors.teal[50],
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: <Widget>[
-                        Container(
-                          child: ListTile(
-                            leading: Icon(Icons.school_outlined, size: 40),
-                            trailing: new Text("24 Penelitian", style: TextStyle(color: Colors.black),),
-                            title: Text(
-                              "Dr. Lala Septem Riza, M.T.",
-                              style:
-                                  const TextStyle( fontSize: 16, color: Colors.black54),
-                            ),
-                            subtitle: Text("Ilmu Komputer",
-                              style:
-                                  TextStyle(fontSize: 14, color: Colors.black)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  componentCard(
+                      "Prof. Dr. Munir, M. IT.", "30", "Ilmu Komputer", true),
+                  componentCard(
+                      "Dr. Yudi Wibisono, M.T.", "27", "Ilmu Komputer", false),
+                  componentCard("Dr. Muhammad Nursalman, M.T.", "26",
+                      "Ilmu Komputer", true),
+                  componentCard("Dr. Lala Septem Riza, M.T.", "22",
+                      "Ilmu Komputer", false),
+                  componentCard("Dra. Cucu Ruhidawati, M.Si", "21",
+                      "Pendidikan Tata Boga", true),
+                  componentCard("Prof. Emi Emilia, M.Ed., Ph.D.", "18",
+                      "Pendidikan Bahasa Inggris", false),
                 ],
               ),
             ),
           ]),
         ),
-        
       ),
     );
   }
