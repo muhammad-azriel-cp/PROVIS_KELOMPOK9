6674ca3cb3692a3822d364c87ca3ea9d2a2a539b
Annisa Muja Ahidah <80079890+annisamujaa@users.noreply.github.com>
2022-06-16 21:20:50 +0700

    Update bottomNav.dart

--- lib/bottom/bottomNav.dart
+++ lib/bottom/bottomNav.dart
@@ -4,21 +4,20 @@ import 'detailFIP.dart';
 import 'universitas.dart';
 import 'home.dart';
 import 'dosen.dart';
-import 'fakultasApp.dart';
+import 'fakultas.dart';
 import 'mahasiswa.dart';
 
 class BottomNav extends StatefulWidget {
-
   @override
   _BottomNavState createState() => _BottomNavState();
 }
 
 class _BottomNavState extends State<BottomNav> {
   int currentIndex = 0;
-  final List<Widget> body =[
+  final List<Widget> body = [
     Home(),
     Dosen(),
-    FakultasApp(),
+    Fakultas(),
     Mhs(),
     Universitas(),
   ];
@@ -28,37 +27,38 @@ class _BottomNavState extends State<BottomNav> {
       body: body[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         onTap: ontap,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_rounded, size: 30),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school_outlined, size: 32),
-              label: 'Dosen',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business_rounded, size: 32),
-              label: 'Fakultas',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.people_outlined, size: 32),
-              label: 'Mahasiswa',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.account_balance_sharp, size: 32),
-              label: 'Universitas',
-            ),
-          ],
-          currentIndex: currentIndex,
-          selectedItemColor: Colors.blueAccent,
-          unselectedItemColor: Colors.grey,
-          showUnselectedLabels: true,
-          //onTap: _changeSelectedNavBar,
-        ),
-      );
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded, size: 30),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school_outlined, size: 32),
+            label: 'Dosen',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business_rounded, size: 32),
+            label: 'Fakultas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_outlined, size: 32),
+            label: 'Mahasiswa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_sharp, size: 32),
+            label: 'Universitas',
+          ),
+        ],
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        //onTap: _changeSelectedNavBar,
+      ),
+    );
   }
-  void ontap(int index){
+
+  void ontap(int index) {
     setState(() {
       currentIndex = index;
     });
